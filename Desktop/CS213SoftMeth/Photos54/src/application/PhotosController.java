38073ff56398007f174e07f98bd3a13efafd3297
Tiffany Moral <tiffany.moral@gmail.com>
2018-11-18 17:08:58 -0500

    setting up file hierarchy

---
+++ Desktop/CS213SoftMeth/Photos54/src/application/PhotosController.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.util.Calendar.*;
+import java.time.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
+import javafx.scene.control.cell.PropertyValueFactory;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.io.ObjectInputStream;
+
+
+/*
+ * Song Library Assignment
+ * CS213 Soft Meth
+ * 
+ * Project Done by:
+ * Tiffany Moral (tm558)
+ * Jake Van Osten (jrv89)
+ * 
+ * 
+ */
+
+
+public class PhotosController {
+
+	
+	
+}
