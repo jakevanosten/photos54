0ace7e01148f57f178214f0ae460d4b748381993
Jake Van Osten <jakevanosten@gmail.com>
2018-11-19 17:18:41 -0500

    Added stock photos and userList file

--- src/data/PhotoLib.java
+++ src/data/PhotoLib.java
@@ -1,24 +1,22 @@
 package data;
 
 import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
-//import data.Photos;
+import javafx.scene.layout.AnchorPane;
+ import javafx.stage.Stage;
+
 
 public class PhotoLib extends Application {
 	@Override
 	public void start(Stage primaryStage) 
 	throws IOException {
-				
+		
 		/*---- Loading in FXML and starting up the scene ----*/
 		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(getClass().getResource("Photos.fxml"));
-		BorderPane root = (BorderPane) loader.load();
+		loader.setLocation(getClass().getResource("photolibrary.fxml"));
+		AnchorPane root = loader.load();
 		
 		PhotosController libraryController = loader.getController();
 		libraryController.start(primaryStage);
