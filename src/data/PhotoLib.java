e023dd9a60e09a62c490281276913784ba9b6e85
Jake Van Osten <jakevanosten@gmail.com>
2018-11-19 17:53:38 -0500

    added login page as primary stage, as well as changeStage method in PhotoLib

--- src/data/PhotoLib.java
+++ src/data/PhotoLib.java
@@ -3,6 +3,7 @@ package data;
 import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
  import javafx.stage.Stage;
@@ -15,7 +16,7 @@ public class PhotoLib extends Application {
 		
 		/*---- Loading in FXML and starting up the scene ----*/
 		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(getClass().getResource("photolibrary.fxml"));
+		loader.setLocation(getClass().getResource("login.fxml"));
 		AnchorPane root = loader.load();
 		
 		PhotosController libraryController = loader.getController();
@@ -23,9 +24,16 @@ public class PhotoLib extends Application {
 		
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
-		primaryStage.setTitle("Photo Library");  
+		primaryStage.setTitle("Photo Library Login");  
 		primaryStage.show();
 		
+	}
+	public void changeScene(String fxml,Stage primaryStage) throws IOException{
+	    Parent pane = FXMLLoader.load(
+	           getClass().getResource(fxml));
+
+	   Scene scene = new Scene( pane );
+	   primaryStage.setScene(scene);
 	}
 	
 	public static void main(String[] args) {
